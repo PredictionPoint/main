6f51b74d2d776dd3bd381a01416b8c67a8ccbd88
ashwinvsinha <ashwinvsinha@gmail.com>
2015-04-26 02:17:32 +0530

    Home page layout structure as per yash's image
    Made a basic homepage layout structure as per Yash's image, so as to
    make it easier for Yash to build thereafter

---
+++ code/index.php
@@ -0,0 +1,324 @@
+<?php
+//our php code for next phase
+?>
+<html>
+<head>
+    <!-- Standard Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    
+    <!-- Site Properities -->
+    <title>Prediction Point</title>
+    <link rel="stylesheet" type="text/css" href="dist/semantic.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!--<script src="js/modernizr.js"></script>-->
+</head>
+<body>
+    <div class="ui grid">
+        <div class="left floated three wide column">
+      <div class="ui segment">
+        LOGO
+      </div>
+    </div>
+        <div class="right floated three wide column">
+      <div class="ui segment">
+        Login or Sign Up
+      </div>
+        </div>
+      <div id="searchBar" class="left floated six wide column">
+      
+     <div class="ui fluid action input">
+  <input type="text" placeholder="Search...">
+  <div class="ui icon button"><i class="search icon"></i></div>
+</div> 
+      </div>
+      </div>
+      <div class="ui grid">
+        <div class="left floated three wide column">
+            <div class="ui segment">
+                LINKS:
+            </div>
+        </div>
+        <div class="left floated ten wide column">
+            <div class="ui segment">
+                <div class="ui divided horizontal list">
+                    <div class="item">SPORTS</div>
+                    <div class="item">Social</div>
+                    <div class="item">Politics</div>
+                    <div class="item">Stock Market</div>
+                </div>
+            </div>
+        </div>
+      </div>
+      
+      <div class="ui grid">
+        <div class="left floated three wide column">
+        <div class="ui segment" id="sideBar">
+                <div class="ui link list">
+                    <div class="active item">How To Play</div>
+                    <a class="item">Rules</a>
+                    <a class="item">Create Groups</a>
+                    <a class="item">Challenge Friends</a>
+                    <a class="item">Custom Bets</a>
+                    <a class="item">Leadership Boards</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="left floated ten wide column">
+            
+        <div class="ui top attached tabular menu">
+            <a class="active item" data-tab="first">HOT</a>
+            <a class="item" data-tab="second">IN PLAY</a>
+            <a class="item" data-tab="third">CRICKET</a>
+            <a class="item" data-tab="fourth">FOOTBALL</a>
+            <a class="item" data-tab="fifth">TENNIS</a>
+        </div>
+        <div class="ui bottom attached active tab segment" data-tab="first">
+        <div class="ui list">
+            <div class="item">
+                <div class="header">CRICKET: <i>(IN PLAY)</i></div>
+                <table class="ui table">
+                    <thead>
+                    <tr>
+                      <th>Match Prediction</th>
+                      <th>Next Batsmen Out</th>
+                      <th>MI Innings Score</th>
+                    </tr>
+                  </thead>
+                <tbody>
+                  <tr>
+                    <td>MI: <div class="ui tiny button">
+                    3/7
+                    </div>
+                    CSK: <div class="ui tiny button">
+                        7/3
+                    </div>
+                    </td>
+                    <td>Simmon: <div class="ui tiny button">
+                    3/7
+                    </div>
+                    Rohit: <div class="ui tiny button">
+                    4/7
+                    </td>
+                    <td>170+: <div class="ui tiny button">
+                    7/6
+                    </div>
+                    170-: <div class="ui tiny button">
+                    7/5
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+                </div>
+                <div class="item">
+                    <div class="header">FOOTBALL: <i>(IN PLAY)</i></div>
+                    <table class="ui table">
+                    <thead>
+                    <tr>
+                      <th>Match Prediction</th>
+                      <th>Next Goal  in 5 mins</th>
+                      <th>Next goal scorer</th>
+                    </tr>
+                  </thead>
+                <tbody>
+                  <tr>
+                    <td>LVR: <div class="ui tiny button">
+                    4/5
+                    </div>
+                    MU: <div class="ui tiny button">
+                    4/6
+                    </div>
+                    </td>
+                    <td>Yes: <div class="ui tiny button">
+                    3/7
+                    </div>
+                    No: <div class="ui tiny button">
+                    4/7
+                    </td>
+                    <td><a>--click for details--</a></td>
+                  </tr>
+                </tbody>
+              </table>
+                </div>
+                <div class="item">
+                    <div class="header">CRICKET:</div>
+                    <table class="ui table">
+                    <thead>
+                    <tr>
+                      <th>Match Prediction</th>
+                      <th>Next Batsmen Out</th>
+                      <th>MI Innings Score</th>
+                    </tr>
+                  </thead>
+                <tbody>
+                  <tr>
+                    <td>MI: <div class="ui tiny button">
+                    3/7
+                    </div>
+                    CSK: <div class="ui tiny button">
+                        7/3
+                    </div>
+                    </td>
+                    <td>Simmon: <div class="ui tiny button">
+                    3/7
+                    </div>
+                    Rohit: <div class="ui tiny button">
+                    4/7
+                    </td>
+                    <td>170+: <div class="ui tiny button">
+                    7/6
+                    </div>
+                    170-: <div class="ui tiny button">
+                    7/5
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+                </div>
+                <div class="item">
+                    <div class="header">TENNIS:</div>
+                    <table class="ui table">
+                    <thead>
+                    <tr>
+                      <th>Match Prediction</th>
+                      <th>Next to win</th>
+                      <th>Will there be deuce in next shit</th>
+                    </tr>
+                  </thead>
+                <tbody>
+                  <tr>
+                    <td>Rodger: <div class="ui tiny button">
+                    4/5
+                    </div>
+                    Murray: <div class="ui tiny button">
+                    4/6
+                    </div>
+                    </td>
+                    <td>Rodger: <div class="ui tiny button">
+                    3/7
+                    </div>
+                    Murray: <div class="ui tiny button">
+                    4/7
+                    </td>
+                    <td>Yes: <div class="ui tiny button">
+                    3/7
+                    </div>
+                    No: <div class="ui tiny button">
+                    4/7
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+                </div>
+            </div>
+        </div>
+        <div class="ui bottom attached tab segment tab" data-tab="second">
+        IN PLAY
+        </div>
+        <div class="ui bottom attached tab segment tab" data-tab="third">
+        CRICKET
+        </div>
+        <div class="ui bottom attached tab segment tab" data-tab="fourth">
+        FOOTBALL
+        </div>
+        <div class="ui bottom attached tab segment tab" data-tab="fifth">
+        TENNIS
+        </div>
+        
+        </div>
+              <div class="left floated three wide column">
+              <div class="ui segment">
+                <div class="ui top attached label">Our Sponsors</div>
+                            <div class="ui divided list">
+              <div class="item">
+             
+                <div class="content">
+                  <a class="header">Watch Hot Indian Girls</a>
+                  <div class="description">Awsome steamy savita bhabhi nude</div>
+                </div>
+              </div>
+              <div class="item">
+                
+                <div class="content">
+                  <a class="header">Awsome savitha Bhabhi going insane</a>
+                  <div class="description">Predict what savitha bhabhi will do next!! Aha</div>
+                </div>
+              </div>
+              <div class="item">
+               
+                <div class="content">
+                  <a class="header">HOT single girls wana meet you right now</a>
+                  <div class="description">Click Here quickly</div>
+                </div>
+              </div>
+              <div class="item">
+                
+                <div class="content">
+                  <a class="header">Win a trip to LA</a>
+                  <div class="description">Tired of mumbai, wana go to LA</div>
+                </div>
+              </div>
+            </div>
+              </div>
+              </div>
+      </div>
+      
+      
+      <div class="ui grid">
+        <div class="left floated three wide column" id="hidden">
+    
+        <div class="ui segment home">
+        <div class="ui big bottom left attached label">1</div>
+        <h1>JOIN</h1>
+      <p>Open your new account, and we will reward you with 1000 points to start with!!</p>
+    </div>
+  </div>
+        <div class="left floated three wide column tiles">
+    
+        <div class="ui segment home">
+        <div class="ui big bottom left attached label">1</div>
+        <h1>JOIN</h1>
+      <p>Open your new account, and we will reward you with 1000 points to start with!!</p>
+    </div>
+  </div>
+  <div class="left floated three wide column tiles">
+    <div class="ui segment home">
+        <div class="ui big bottom left attached label">2</div>
+      <h1>PREDICT</h1>
+      <p>Make your predictions before or during the match on various markets available</p>
+    </div>
+  </div>
+  <div class="left floated three wide column tiles">
+    <div class="ui segment home">
+      <div class="ui big bottom left attached label">3</div>
+      <h1>WIN</h1>
+      <p>Earn points for correct predictions and climb the leaderboards</p>
+    </div>
+  </div>
+</div>
+
+  <div class="ui centered grid">
+  <div class="ui horizontal bulleted link list" id="footer">
+  <a class="item">
+    Terms and Conditions
+  </a>
+  <a class="item">
+    Privacy Policy
+  </a>
+  <a class="item">
+    Contact Us
+  </a>
+  <div class="item">
+    &copy; 2015 Prediction Point
+  </div>
+</div>
+   </div>
+    
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.js"></script>
+<!--<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery.address/1.6/jquery.address.min.js"></script>-->
+<script src="dist/semantic.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ code/js/script.js
@@ -0,0 +1,5 @@
+
+//for homepage tab
+$('.menu .item')
+  .tab()
+;
